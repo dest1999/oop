af8a314a9c6a01e0167059c39abecaceac972768
Oleg <serv_all@mail.ru>
2021-10-08 01:02:44 +0300

    1-я часть задания выполнена

---
+++ Lesson2/Lesson2/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Lesson2
+{
+
+
+    internal class Account
+    {
+        private int number;
+        private decimal balance;
+        private AccountTypes accountType;
+
+        public int GetNumber()
+        {
+            return number;
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+
+        public AccountTypes GetAccountType()
+        {
+            return accountType;
+        }
+
+        public void SetNumber(int Number)
+        {
+            this.number = Number;
+        }
+
+        public void SetBalance(decimal Balance)
+        {
+            this.balance = Balance;
+        }
+
+        public void SetAccountType(AccountTypes AccountType)
+        {
+            this.accountType = AccountType ;
+        }
+    }
+}
