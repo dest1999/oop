16de80e0ad72d1d6fb9805a38c07b5b9313b1492
Oleg <serv_all@mail.ru>
2021-10-18 22:11:23 +0300

    Добавлен метод перевода со счёта на счёт

---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+//TODO В класс банковский счет, созданный в упражнениях, добавить метод, который переводит деньги с одного счета на другой.
+//У метода два параметра: ссылка на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма.
+
+namespace Lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new Account(100);
+            var account2 = new Account(5000);
+
+            Console.WriteLine(account1.GetInfo());
+            Console.WriteLine(account2.GetInfo());
+
+            Console.WriteLine($"Transaction success: {account1.Transfer(account2, 5000)}");
+
+            Console.WriteLine(account1.GetInfo());
+            Console.WriteLine(account2.GetInfo());
+        }
+    }
+}
