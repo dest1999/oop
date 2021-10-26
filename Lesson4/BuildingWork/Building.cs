c61aacf75ff6e905950ca4f2bc00263f40e5c422
Oleg <serv_all@mail.ru>
2021-10-26 23:21:59 +0300

    Задание 1 к уроку 4 выполнено

---
+++ Lesson4/BuildingWork/Building.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingWork
+{
+    internal class Building
+    {
+        static private int buildCounter;
+        
+        private int buildNumber;
+        private double height;
+        private int floorsQuantity;
+        private int flatsQuantity;
+        private int entrancesQuantity;
+
+        public double Height
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+            }
+        }
+
+        public int FloorsQuantity
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    floorsQuantity = value;
+                }
+            }
+        }
+
+        public int FlatsQuantity
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    flatsQuantity = value;
+                }
+            }
+        }
+
+        public int EntrancesQuantity
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    entrancesQuantity = value;
+                }
+            }
+        }
+
+        public Building()
+        {
+            SetBuildNumber();
+        }
+
+
+        public double FloorHeight()
+        {
+            return height / floorsQuantity;
+        }
+
+        public int FlatsInEntrance()
+        {
+            return flatsQuantity / entrancesQuantity;
+        }
+
+        public int FlatsInFloor()
+        {
+            return FlatsInEntrance() / floorsQuantity;
+        }
+
+        public override string ToString()
+        {
+            return 
+                $"Building No: {buildNumber}\n" +
+                $"Building height: {height}\n" +
+                $"Building floors: {floorsQuantity}\n" +
+                $"Building flats: {flatsQuantity}\n" +
+                $"Building entrances: {entrancesQuantity}";
+        }
+
+        private void SetBuildNumber()
+        {
+            buildNumber = ++buildCounter;
+        }
+
+    }
+}
---
+++ Lesson4/BuildingWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BuildingWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var build1 = new Building
+            {
+                EntrancesQuantity = 1,
+                FlatsQuantity = 1,
+                FloorsQuantity = 1,
+                Height = 10
+            };
+
+            var build2 = new Building
+            {
+                EntrancesQuantity = 2,
+                FlatsQuantity = 2,
+                FloorsQuantity = 2,
+                Height = 20
+            };
+
+            Console.WriteLine(build1 + "\n");
+            Console.WriteLine(build2);
+        }
+    }
+}
