c074e9a2fcfaa4f37148fa3ce627bb9951df0ec9
Oleg <serv_all@mail.ru>
2021-11-11 01:29:08 +0300

    Тестирование для класса комплексных чисел выполнено

--- Lesson5/ComplexNumbers/Program.cs
+++ Lesson5/ComplexNumbers/Program.cs
@@ -8,10 +8,7 @@ namespace ComplexNumbers
         {
             var complexNumber = new ComplexNumber(3, "2i");
             var complexNumber2 = new ComplexNumber(Imaginary:-3);
-            var complexNumber3 = new ComplexNumber();
-            Console.WriteLine(complexNumber * complexNumber2);//
-            Console.WriteLine(complexNumber2);
-            Console.WriteLine(complexNumber3);
+            Console.WriteLine(complexNumber * complexNumber2);
         }
     }
 }
