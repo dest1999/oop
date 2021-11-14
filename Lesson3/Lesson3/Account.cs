99da513e0f8fe1e2f6875170e568ca13f68598b1
Oleg <serv_all@mail.ru>
2021-11-14 23:05:59 +0300

    Для класса банковский счёт переопределены операторы == !=, методы Equals, GetHashCode, ToString. Функционал проверен на простом примере (юнит-тесты не создавались)

--- Lesson3/Lesson3/Account.cs
+++ Lesson3/Lesson3/Account.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson3
+namespace Lesson6
 {
     internal class Account
     {
@@ -35,6 +35,21 @@ namespace Lesson3
             this.accountType = accountType;
             SetAccountNumber();
         }
+        
+        public static bool operator == (Account account1, Account account2)
+        {
+
+            if (account1.accountNumber == account2.accountNumber && account1.accountType == account2.accountType && account1.balance == account2.balance)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator != (Account account1, Account account2)
+        {
+            return !(account1 == account2);
+        }
 
         public int AccountNumber
         {
@@ -51,10 +66,6 @@ namespace Lesson3
             {
                 return balance;
             }
-            //set 
-            //{  
-            //    balance = value; 
-            //}  
         }
 
         public AccountTypes AccountType
@@ -71,7 +82,7 @@ namespace Lesson3
 
         public bool Transfer(Account sourceAccount, decimal amount)
         {
-            if (sourceAccount != null && sourceAccount.WithdrawFromAccount(amount))
+            if (sourceAccount.WithdrawFromAccount(amount))
             {
                 return this.DepositToAccount(amount);
             }
@@ -98,9 +109,27 @@ namespace Lesson3
             return false;
         }
 
-        public string GetInfo()
+        public override string ToString()
         {
             return $"Счёт номер\t{accountNumber}\nБаланс\t\t{balance}\nТип\t\t{accountType}\n";
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = new HashCode();
+            hash.Add(accountNumber);
+            hash.Add(balance);
+            hash.Add(accountType);
+            return hash.ToHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj is not Account)
+            {
+                return false;
+            }
+            return this == (Account)obj;
         }
 
         private void SetAccountNumber()
--- Lesson3/Lesson3/AccountTypes.cs
+++ Lesson3/Lesson3/AccountTypes.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson3
+namespace Lesson6
 {
     enum AccountTypes
     {
