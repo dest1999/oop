f5a2fb33f7cbb25f7b1db945a8f82915531044b8
Oleg <serv_all@mail.ru>
2021-11-17 00:39:49 +0300

    Создан класс Figure и его наследование: Point, Circle, Rectangle

---
+++ Lesson6/Figure/Entityes/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureProject
+{
+    internal class Point : Figure
+    {
+        protected static int coefficient = 2;//поправка для приведения "консольного пикселя" к квадрату
+
+        public virtual void Draw()
+        {
+            if (IsVisible)
+            {
+                Console.SetCursorPosition(horisontalPosition, verticalPosition);
+                Console.ForegroundColor = Color;
+                Console.WriteLine("*");
+                Console.ResetColor();
+            }
+        }
+
+    }
+}
