abd879ec2bdaf73c207e59ceed9ab42f33222457
Oleg <serv_all@mail.ru>
2021-11-22 23:32:20 +0300

    Класс ACoder отрефакторен для достижения DRY

--- Lesson7/Coder/Coder/Entityes/ACoder.cs
+++ Lesson7/Coder/Coder/Entityes/ACoder.cs
@@ -10,64 +10,48 @@ namespace Coder
     {
         public string Encode(string flatString)
         {
-            StringBuilder sb = new ();
+            return ConvertMethod(flatString, true);
+        }
+        public string Decode(string codedString)
+        {
+            return ConvertMethod(codedString, false);
+        }
+
+        private string ConvertMethod(string inputString, bool needToEncode)
+        {
+            StringBuilder sb = new();
             int index;
             bool symbolFound;
-            foreach (char item in flatString)
+            foreach (char currentSymbol in inputString)
             {
                 symbolFound = false;
                 for (int i = 0; i < Alphabeth.Symbols.Length; i++)
                 {
-                    if (Alphabeth.Symbols[i].Contains(item))
+                    if (Alphabeth.Symbols[i].Contains(currentSymbol))
                     {
-                        index = Alphabeth.Symbols[i].IndexOf(item);
-                        if(++index > Alphabeth.Symbols[i].Length - 1)
+                        index = Alphabeth.Symbols[i].IndexOf(currentSymbol);
+
+                        if (needToEncode && (++index > Alphabeth.Symbols[i].Length - 1))
                         {
                             index = 0;
                         }
+                        else if (!needToEncode && (--index < 0))
+                        {
+                            index = Alphabeth.Symbols[i].Length - 1;
+                        }
+
                         sb.Append(Alphabeth.Symbols[i][index]);
                         symbolFound = true;
                         break;
                     }
                 }
-                if (!symbolFound) 
-                { 
-                    sb.Append(item); 
-                }
-            }
-
-            return sb.ToString();
-        }
-
-        public string Decode(string codedString)
-        {
-            StringBuilder sb = new ();
-            int index;
-            bool symbolFound;
-            foreach (char item in codedString)
-            {
-                symbolFound = false;
-                for (int i = 0; i < Alphabeth.Symbols.Length; i++)
-                {
-                    if (Alphabeth.Symbols[i].Contains(item))
-                    {
-                        index = Alphabeth.Symbols[i].IndexOf(item);
-                        if (--index < 0)
-                        {
-                            index = Alphabeth.Symbols[i].Length - 1;
-                        }
-                        sb.Append(Alphabeth.Symbols[i][index]);
-                        symbolFound=true;
-                        break;
-                    }
-                }
                 if (!symbolFound)
                 {
-                    sb.Append(item);
+                    sb.Append(currentSymbol);
                 }
             }
+
             return sb.ToString();
         }
-
     }
 }
