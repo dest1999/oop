e5d9f5b914cd99e7d87bf61a05a45bf145367ecd
Oleg <serv_all@mail.ru>
2021-11-22 22:40:27 +0300

    Часть 2 выполнена, введен интерфейс IArea

---
+++ Lesson7/Figure/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FigureProject
+{
+    internal class Program
+    {
+        public static int BufferHeight = 0;
+        static void Main(string[] args)
+        {
+            Point point = new();
+            point.SetColorTo(ConsoleColor.Green);
+            point.Draw();
+            point.MoveToPosition(2, 3);
+            point.Draw();
+
+            Circle circle = new(10, 10, 14);
+            circle.SetColorTo(ConsoleColor.Yellow);
+            circle.Visible(true);
+            circle.Draw();
+
+            Rectangle rectangle = new(8, 21, 9, 4);
+            rectangle.Visible(true);
+            rectangle.SetColorTo(ConsoleColor.Gray);
+            rectangle.Draw();
+
+            Console.SetCursorPosition(0, BufferHeight);
+            Console.WriteLine(point);
+            Console.WriteLine(circle);
+            Console.WriteLine(rectangle);
+        }
+    }
+}
